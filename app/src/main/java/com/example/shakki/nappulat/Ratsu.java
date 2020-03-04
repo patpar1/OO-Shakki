8827c13174e43c094d21ef17e08e60058a38c71c
Sriracha22 <sakari.miettinen@student.lut.fi>
2020-03-04 11:46:53 +0200

    lisätty siirtoehdokkaat ja aloitettu laillisetSiirrot

--- app/src/main/java/com/example/shakki/nappulat/Ratsu.java
+++ app/src/main/java/com/example/shakki/nappulat/Ratsu.java
@@ -3,12 +3,37 @@ package com.example.shakki.nappulat;
 import java.util.ArrayList;
 
 public class Ratsu extends Ylänappula {
+
+    private static final int[][] siirtoEhdokkaat = {
+            {-2, -1},
+            {-1, -2},
+            {1, -2},
+            {2, -1},
+            {2, 1},
+            {1, 2},
+            {-1, 2},
+            {-2, 1}
+    };
+
     public Ratsu(boolean onValkoinen) {
         super(onValkoinen);
     }
 
     @Override
     public ArrayList<Siirto> laillisetSiirrot(Pelilauta lauta, int x, int y) {
-        return null;
+        ArrayList<Siirto> siirtoLista = new ArrayList<Siirto>();
+
+        for (int[] siirto : siirtoEhdokkaat) {
+            int[] siirtoEhdokas = {(x + siirto[0]), (y + siirto[1])};
+
+            //TODO if, elseif, else lausekkeet tähän
+
+            siirtoLista.add(new Siirto());
+        }
+
+
+
+        return siirtoLista;
+
     }
 }

51dadd9a4367983712eb5848256be163822e3aa4
patrik <59889312+patpar1@users.noreply.github.com>
2020-03-04 19:40:30 +0200

    Muutettu siirto-luokan toiminnallisuutta

--- app/src/main/java/com/example/shakki/Siirto.java
+++ app/src/main/java/com/example/shakki/Siirto.java
@@ -4,19 +4,45 @@ import com.example.shakki.nappulat.Nappula;
 
 public class Siirto {
 
-    private Nappula nappula;
-    private Ruutu aloitusRuutu;
-    private Ruutu lopetusRuutu;
+    private int x0;
+    private int y0;
+    private int x1;
+    private int y1;
 
-    private boolean onLyönti;
+    /* private boolean onLyönti;
     private boolean onShakki;
     private boolean onMatti;
-    private boolean linnoitus;
+    private boolean linnoitus; */
 
-    public Siirto(Nappula nappula, Ruutu aloitusRuutu, Ruutu lopetusRuutu) {
-        this.nappula = nappula;
-        this.aloitusRuutu = aloitusRuutu;
-        this.lopetusRuutu = lopetusRuutu;
+    public int getX0() {
+        return x0;
     }
 
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY0() {
+        return y0;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setX0(int x0) {
+        this.x0 = x0;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public void setY0(int y0) {
+        this.y0 = y0;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
 }

985618272a07ab819278bfd39d18bcf5c6d30a32
patrik <59889312+patpar1@users.noreply.github.com>
2020-03-04 19:40:55 +0200

    Lisätty ruutu-luokkaan ruudun koordinaatit.

--- app/src/main/java/com/example/shakki/Ruutu.java
+++ app/src/main/java/com/example/shakki/Ruutu.java
@@ -5,17 +5,35 @@ import com.example.shakki.nappulat.Nappula;
 public class Ruutu {
 
     private Nappula nappula;
+    private int x;
+    private int y;
 
-    public Ruutu() {
+    public Ruutu(int y, int x) {
         nappula = null;
+        this.x = x;
+        this.y = y;
     }
 
-    public Ruutu(Nappula nappula) {
+    public Ruutu(Nappula nappula, int y, int x) {
         this.nappula = nappula;
+        this.x = x;
+        this.y = y;
     }
 
     public Nappula getNappula() {
         return nappula;
+    }
+
+    public void setNappula(Nappula nappula) {
+        this.nappula = nappula;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public boolean onTyhjä() {

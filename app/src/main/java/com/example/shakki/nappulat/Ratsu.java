0077d9a76d0717a3d1f3ce16277d2831f05e1e7f
Sriracha22 <sakari.miettinen@student.lut.fi>
2020-03-04 11:21:40 +0200

    lisätty ratsu.java ja ylänappulan toiminnallisuutta

---
+++ app/src/main/java/com/example/shakki/nappulat/Ratsu.java
@@ -0,0 +1,14 @@
+package com.example.shakki.nappulat;
+
+import java.util.ArrayList;
+
+public class Ratsu extends Ylänappula {
+    public Ratsu(boolean onValkoinen) {
+        super(onValkoinen);
+    }
+
+    @Override
+    public ArrayList<Siirto> laillisetSiirrot(Pelilauta lauta, int x, int y) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/shakki/nappulat/Ylänappula.java
+++ app/src/main/java/com/example/shakki/nappulat/Ylänappula.java
@@ -5,6 +5,20 @@ import java.util.ArrayList;
 public abstract class Ylänappula {
     private boolean onValkoinen;
 
+    public Ylänappula (boolean onValkoinen) {
+        self.onValkoinen = onValkoinen;
+    }
+//Laskee lailliset siirrot. Palauttaa listan laillisista siirroista
     public abstract ArrayList<Siirto> laillisetSiirrot(Pelilauta lauta, int x, int y);
+
+    public static boolean onLaudalla(int x, int y) {
+        if ((x > 7) || (y > 7)) {
+            return false;
+        }
+        if ((x < 0) || (y < 0)) {
+            return false;
+        }
+        return true;
+    }
 
 }

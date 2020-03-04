3b5b357e11cbfddbdf1cf8699f6b2c266fad1032
patrik <59889312+patpar1@users.noreply.github.com>
2020-03-04 17:34:01 +0200

    Lisätty Peli.java ja alustava toiminnallisuus.

---
+++ app/src/main/java/com/example/shakki/Peli.java
@@ -0,0 +1,14 @@
+package com.example.shakki;
+
+import java.util.ArrayList;
+
+public class Peli {
+
+    private Pelilauta lauta;
+    private ArrayList<Siirto> siirrot;
+
+    public Peli() {
+        lauta = new Pelilauta();
+    }
+
+}

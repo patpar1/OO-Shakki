191ac13d7b3e7754c57d91267c1e3396b6f90eba
Sriracha22 <sakari.miettinen@student.lut.fi>
2020-03-04 10:55:43 +0200

    lisätty ylänappulan toiminnallisuutta

--- app/src/main/java/com/example/shakki/nappulat/Ylänappula.java
+++ app/src/main/java/com/example/shakki/nappulat/Ylänappula.java
@@ -1,4 +1,10 @@
 package com.example.shakki.nappulat;
 
+import java.util.ArrayList;
+
 public abstract class Ylänappula {
+    private boolean onValkoinen;
+
+    public abstract ArrayList<Siirto> laillisetSiirrot(Pelilauta lauta, int x, int y);
+
 }

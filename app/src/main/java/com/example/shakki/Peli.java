e1faada30460b17dec6cb9b5ae3d122948edc0a0
patrik <59889312+patpar1@users.noreply.github.com>
2020-03-11 17:55:18 +0200

    Jatkettu pelitiedoston toimintaa

--- app/src/main/java/com/example/shakki/Peli.java
+++ app/src/main/java/com/example/shakki/Peli.java
@@ -6,9 +6,20 @@ public class Peli {
 
     private Pelilauta lauta;
     private ArrayList<Siirto> siirrot;
+    private boolean valkoisenVuoro;
 
     public Peli() {
         lauta = new Pelilauta();
+        siirrot = new ArrayList<Siirto>();
+        valkoisenVuoro = true;
+    }
+
+    public int peliSilmukka() {
+        while (true) {
+            // Tarkista siirrot
+            // Tee siirrto
+            this.valkoisenVuoro = !valkoisenVuoro; // Anna vuoro toiselle pelaajalle
+        }
     }
 
 }

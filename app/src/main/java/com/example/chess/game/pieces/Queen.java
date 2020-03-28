8d6ada89bb80fec84c614975e6235b14c4351ebd
Sriracha22 <sakari.miettinen@student.lut.fi>
2020-03-28 19:17:09 +0200

    Kuningatar valmis

--- app/src/main/java/com/example/chess/game/pieces/Queen.java
+++ app/src/main/java/com/example/chess/game/pieces/Queen.java
@@ -7,13 +7,44 @@ import java.util.ArrayList;
 
 public class Queen extends Piece {
 
+    private static final int[][] moveVectors = {
+            {-1, 0}, // Up
+            {1, 0}, // Down
+            {0, -1}, // Left
+            {0, 1}, // Right
+            {-1, 1}, // Right Up
+            {1, 1}, // Right Down
+            {-1, -1}, // Left Up
+            {1, -1} // Left Down
+    };
+
     public Queen(boolean isWhite) {
         super(isWhite);
     }
 
     @Override
     public ArrayList<Square> legalMoves(Board board, int row, int col) {
-        return null;
+        ArrayList<Square> moveArray = new ArrayList<Square>();
+        for (int[] direction : moveVectors) {
+            int[] moveCandidate = {row, col};
+            while (true) {
+                moveCandidate[0] += direction[0];
+                moveCandidate[1] += direction[1];
+                if (!isOnBoard(moveCandidate[0], moveCandidate[1])) {
+                    break;
+                }
+                if (board.getSquare(moveCandidate[0], moveCandidate[1]).hasPiece()){
+                    if (this.isWhite() == board.getSquare(moveCandidate[0], moveCandidate[1]).getPiece().isWhite()) {
+                        break;
+                    } else {
+                        moveArray.add(board.getSquare(moveCandidate[0] , moveCandidate[1]));
+                        break;
+                    }
+                }
+                moveArray.add(board.getSquare(moveCandidate[0] , moveCandidate[1]));
+            }
+        }
+        return moveArray;
     }
 
     @Override
